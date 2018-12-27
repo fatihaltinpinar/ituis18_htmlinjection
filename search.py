583bee130b58c1021c2ae0d336a3304ba26fb5f8
faati <fatihaltinpinar@gmail.com>
2018-12-27 17:37:36 +0300

    Creating the structure of the code

---
+++ search.py
@@ -0,0 +1,16 @@
+# Function that finds readme:
+def find_readme(repository):
+
+# Function that finds readme file
+def find_heroku_link(readme_file):
+
+# Function that finds all .py files in a given repository
+def find_py_files(repository):
+
+def find_post_method(file_list):
+
+
+
+
+if __name__ == '__main__':
+    find_readme('a2-abdinli18')
